ed77f8238c446f1caa8a5ec060f5f17028453bb9
minnje97 <dlalswp112@naver.com>
2022-05-01 02:35:37 +0900

    220429

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,5 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
+import authService from "../fbase";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
+import { async } from "@firebase/util";
 
-const Auth = () => <span>Auth</span>;
+const Auth = () => {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [newAccount, setNewAccount] = useState(true);
+  const onChange = (event) => {
+    const {
+      target: { name, value },
+    } = event;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      let data;
+      const auth = getAuth();
+      if (newAccount) {
+        data = await createUserWithEmailAndPassword(auth, email, password);
+      } else {
+        data = await signInWithEmailAndPassword(auth, email, password);
+      }
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          required
+          value={email}
+          onChange={onChange}
+        ></input>
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          required
+          value={password}
+          onChange={onChange}
+        ></input>
+        <input
+          type="submit"
+          value={newAccount ? "Create Account" : "Login"}
+          required
+        ></input>
+      </form>
+      <div>
+        <button>Continue with Google</button>
+        <button>Continue with Github</button>
+      </div>
+    </div>
+  );
+};
 
 export default Auth;
